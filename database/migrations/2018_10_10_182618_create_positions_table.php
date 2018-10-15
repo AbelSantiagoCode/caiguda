8f5b2d7fd02957888e756b58579587481c78a254
AbelSantiagoCode <asbgabel@gmail.com>
2018-10-15 20:21:00 +0200

    commit

---
+++ database/migrations/2018_10_10_182618_create_positions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePositionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('positions', function (Blueprint $table) {
+          $table->unsignedInteger('id');
+          $table->primary('id');
+          $table->string('sector_id',10);
+          $table->foreign('sector_id')->references('id')->on('sectors')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('positions');
+    }
+}
---
+++ database/migrations/2018_10_10_182746_create_devices_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDevicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+          $table->unsignedInteger('id');
+          $table->primary('id');
+          $table->unsignedInteger('position_id');
+          $table->foreign('position_id')->references('id')->on('positions')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devices');
+    }
+}
